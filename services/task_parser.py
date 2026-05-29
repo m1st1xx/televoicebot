576be257b8cf82cdf75919afb59ab9e7b0f9ff3c
m1st1xx <sashabalandin14@gmail.com>
2026-05-30 00:48:31 +0300

    edited services/task_parser.py Added declensions and name recognition using AI model.

--- services/task_parser.py
+++ services/task_parser.py
@@ -1,23 +1,92 @@
 import re
 
+from natasha import (
+    Segmenter,
+    MorphVocab,
+    NewsEmbedding,
+    NewsNERTagger,
+    Doc
+)
 
-def parse_tasks(text):
+import pymorphy2
 
-    pattern = r'(\w+)[её]?\s+нужно\s+([^\.]+)'
 
-    matches = re.findall(
-        pattern,
-        text,
-        flags=re.IGNORECASE
-    )
+segmenter = Segmenter()
+morph_vocab = MorphVocab()
+emb = NewsEmbedding()
+ner_tagger = NewsNERTagger(emb)
+morph = pymorphy2.MorphAnalyzer()
 
+def normalize_name(name):
+    parsed = morph.parse(name)[0]
+    return parsed.normal_form
+
+
+def extract_tasks(text):
+    doc = Doc(text)
+    doc.segment(segmenter)
+    doc.tag_ner(ner_tagger)
     tasks = []
+    sentences = re.split(r'[.!?]', text)
 
-    for name, task in matches:
+    for sentence in sentences:
+        sentence = sentence.strip()
+
+        if not sentence:
+            continue
+        local_doc = Doc(sentence)
+        local_doc.segment(segmenter)
+        local_doc.tag_ner(ner_tagger)
+        person_name = None
+
+        for span in local_doc.spans:
+
+            if span.type == "PER":
+                span.normalize(morph_vocab)
+                person_name = normalize_name(
+                    span.text
+                )
+                break
+
+        if not person_name:
+            continue
+
+        task_text = sentence
+        task_text = re.sub(
+            person_name,
+            "",
+            task_text,
+            flags=re.IGNORECASE
+        )
+
+        task_text = re.sub(
+            r'^\s*,?',
+            '',
+            task_text
+        ).strip()
+
+        replacements = {
+            "сделай": "сделать",
+            "доработай": "доработать",
+            "заполни": "заполнить",
+            "напиши": "написать",
+            "создай": "создать"
+        }
+
+        words = task_text.split()
+
+        if words:
+
+            first_word = words[0].lower()
+
+            if first_word in replacements:
+                words[0] = replacements[first_word]
+
+        task_text = " ".join(words)
 
         tasks.append({
-            "name": name,
-            "task": task.strip()
+            "name": person_name,
+            "task": task_text
         })
 
     return tasks
