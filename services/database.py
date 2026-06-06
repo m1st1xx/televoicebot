79208299c154dc74bce5e0b61c354675db87ae31
m1st1xx <sashabalandin14@gmail.com>
2026-06-06 22:18:20 +0300

    added new functions in database.py added function update_sheet to add links in db added function get_sheet_id to get link from db added func has_sheet to check link exist in table or not

--- services/database.py
+++ services/database.py
@@ -1,7 +1,7 @@
-import _sqlite3
+import sqlite3
 
 
-conn = _sqlite3.connect("/app/employees.db")
+conn = sqlite3.connect("/app/employees.db")
 cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS employees(
@@ -13,6 +13,14 @@ CREATE TABLE IF NOT EXISTS employees(
 
 conn.commit()
 
+try:
+    cursor.execute("""
+    ALTER TABLE employees
+    ADD COLUMN google_sheet_id TEXT
+    """)
+    conn.commit()
+except sqlite3.OperationalError:
+    pass
 
 def add_employee(name,telegram_id,username):
     cursor.execute("""
@@ -28,3 +36,34 @@ def get_employee(name):
     FROM employees
     WHERE name =?""",(name.lower(),))
     return cursor.fetchone()
+
+def update_sheet_id(telegram_id,google_sheet_id):
+    cursor.execute("""
+        UPDATE employees
+        SET google_sheet_id = ?
+        WHERE telegram_id = ?
+        """,
+        (google_sheet_id, telegram_id)
+    )
+
+    conn.commit()
+
+def get_sheet_id(telegram_id):
+    cursor.execute("""
+        SELECT google_sheet_id
+        FROM employees
+        WHERE telegram_id = ?
+        """,(telegram_id,)
+    )
+
+    row = cursor.fetchone()
+
+    if row:
+        return row[0]
+
+    return None
+
+def has_sheet(telegram_id):
+    sheet_id = get_sheet_id(telegram_id)
+
+    return bool(sheet_id)
